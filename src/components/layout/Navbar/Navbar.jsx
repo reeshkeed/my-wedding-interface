05eb494dcaed0734731aa4289bf33c010c72739d
reeshkeed <hello@girishdaloso.com>
2022-01-12 01:50:31 +0800

    Add on scroll navbar function

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
-import { Container, Image, Button, EmailIcon } from '@chakra-ui/react';
+import { Image, Button } from '@chakra-ui/react';
 import { Link } from 'react-scroll';
+import { useState, useEffect } from 'react';
 import logo from '../../../assets/logo.png';
 import './Navbar.css';
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="navbar d-flex space-between">
-      <div className="d-flex">
+    <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
+      <div className="d-flex v-center">
         <Link className="nav-link" to="home" spy={true} smooth={true}>
           <Image className="logo" src={logo} />
         </Link>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Box, Center } from '@chakra-ui/react';
 import './Home.css';
+
 const Home = () => {
   return (
-    <div id="home">
-      <h1>Home Page</h1>
-    </div>
+    <Center id="home" className="flex-column">
+      <p className="font-cormorant">We're Getting Married!</p>
+      <Box my={20}>
+        <h1 className="our-name font-amalfi">Shane & Girish</h1>
+      </Box>
+      <p>1.30.22</p>
+    </Center>
   );
 };
+
 export default Home;
