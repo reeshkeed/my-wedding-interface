986d8b3d1eb283ef5f7afa6b055ff678a33c1d0a
reeshkeed <hello@girishdaloso.com>
2022-01-18 16:56:08 +0800

    Update login and logout button

--- src/components/layout/Navbar/NavbarButton.jsx
+++ src/components/layout/Navbar/NavbarButton.jsx
@@ -2,9 +2,9 @@ import { Button } from "@chakra-ui/react";
 
 export const NavbarButton = (props) =>
   !props.token ? (
-    <Button onClick={props.onOpen}>Login</Button>
+    <Button colorScheme={'whiteAlpha'} variant={'solid'} onClick={props.onOpen}>Login</Button>
   ) : (
-    <Button variant="ghost" onClick={props.logout}>
+    <Button colorScheme={'whiteAlpha'} variant="ghost" onClick={props.logout}>
       Logout
     </Button>
   );
